2c4b88423ff91e5f9d19f0190ea1b211b17356ae
saicharanN007 <69344375+saicharanN007@users.noreply.github.com>
2020-08-28 20:47:05 +0530

    created matrix.java
    it is program for addition of matrix

---
+++ matrix.java
@@ -0,0 +1,31 @@
+package matrix;
+
+/**
+ *
+ * @author navee
+ */
+public class Matrix {
+
+   
+    public static void main(String[] args) {
+        int[][] a ={{1,3,4},{2,4,3},{3,4,5}} ;
+        int[][] b ={{1,3,4},{2,4,3},{1,2,4}} ;
+        int[][] c = new int[3][3];
+         
+        
+        for(int i = 0; i<a.length; i++){
+            for(int j = 0; j<b.length; j++){
+                c[i][j] = a[i][j] + b[i][j] ;           
+            }
+        }
+    
+    for(int i = 0; i<a.length; i++){
+            for(int j = 0; j<b.length; j++){
+                System.out.println(c[i][j] + " ");
+               
+            }
+            System.out.println();
+    }
+}
+    
+}
