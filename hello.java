5070b39e1514f105a483052b019f1be8ed7fee0f
saicharanN007 <69344375+saicharanN007@users.noreply.github.com>
2022-03-21 20:01:30 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 package firstprogram;
 public class Firstprogram{
     public static void main(String[] args){
-        System.out.println("helo world");
+        System.out.println("hello world");
         }
     }
